54d074682de73afcb484968a1d754e82da230ca5
Debmalya Panday <debmalya.panday@gmail.com>
2025-07-12 16:16:04 +0530

    Update site content and styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,13 @@
 module.exports = function(eleventyConfig) {
   // Passthrough copy for assets
-  eleventyConfig.addPassthroughCopy("debmalyapanday.github.io/css");
-  eleventyConfig.addPassthroughCopy("debmalyapanday.github.io/js");
-  eleventyConfig.addPassthroughCopy("debmalyapanday.github.io/images");
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/images");
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
   return {
     dir: {
-      input: "debmalyapanday.github.io",
+      input: "src",
       output: "_site"
     }
   };
